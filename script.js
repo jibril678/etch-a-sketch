7737f908e29dc6750c76bca6c8b2309dc1d94d62
Jibril <jibrilabdi01@gmail.com>
2023-11-08 20:53:51 +0000

    JS code updates and HTML/CSS styling updates

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const gridContainer = document.querySelector('.grid-container')
-const resetGridButton = document.querySelector('.reset-grid')
-const slider = document.querySelector('.slider')
-const sliderValue = document.querySelector('.sliderValue')
-const colourSelection = document.querySelector('.colour-pick')
-const randomColour = document.querySelector('.random-colour')
+// Button Selections and Global Variables
+const gridContainer = document.querySelector('.grid-container');
+const resetGridButton = document.querySelector('#clear');
+const slider = document.querySelector('.slider');
+const sliderValue = document.querySelector('.sliderValue');
+const colourSelection = document.querySelector('.colour-pick');
+const randomColour = document.querySelector('#rainbow-mode');
+const colourMode = document.querySelector('#colour-mode');
+// Set default colour mode
+let currentColourMode = 'default';
+// Initialise mouseover status
+let isMouseDown = false;
 
+// Function to update colour mode
+function changeColourMode(newColourMode) {
+  currentColourMode = newColourMode;
+}
 
-
-function createGrid(cells, colourSelection) {;
+// Function to create a new grid. The value of 'cells' will be determined by the slider value
+function createGrid(cells) {;
     gridContainer.innerHTML = '';
     
     gridContainer.style.gridTemplateColumns = `repeat(${cells}, 1fr)`;
@@ -18,47 +28,42 @@ function createGrid(cells, colourSelection) {;
       gridItem.classList.add('grid-item');
       gridContainer.appendChild(gridItem);
       gridItem.addEventListener('mouseover', () => {
-        gridItem.style.backgroundColor = `${colourSelection.value}`
-      })
+        if (isMouseDown) {
+          if (currentColourMode === 'default') {
+              gridItem.style.backgroundColor = `${colourSelection.value}`
+          } else if (currentColourMode === 'rainbow') {
+              gridItem.style.backgroundColor = getRandomColour();
+          }
+      }})
     }
   }
 
-  createGrid(slider.value, colourSelection);
-
+// Functions
   function getRandomColour() {
-    let r = Math.floor(Math.random() * 256)
-    let g = Math.floor(Math.random() * 256)
-    let b = Math.floor(Math.random() * 256)
-    const rgb = `rgb(${r},${g},${b})`
-    return rgb
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    const rgb = `rgb(${r},${g},${b})`;
+    return rgb;
   }
 
-  function createGridRandom(cells) {;
+  function resetGrid () {
     gridContainer.innerHTML = '';
-    
-    gridContainer.style.gridTemplateColumns = `repeat(${cells}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${cells}, 1fr)`;
-
-    for (let i = 0; i < cells * cells; i++) {
-      const gridItem = document.createElement('div');
-      gridItem.classList.add('grid-item');
-      gridContainer.appendChild(gridItem);
-      gridItem.addEventListener('mouseover', () => {
-        gridItem.style.backgroundColor = getRandomColour()
-      })
-    }
+    createGrid(slider.value);
+    sliderValue.textContent = slider.value;
   }
 
+
+// Event Listeners
+  resetGridButton.addEventListener('click', resetGrid);
+  colourMode.addEventListener('click', () => changeColourMode('default'));
+  randomColour.addEventListener('click', () => changeColourMode('rainbow'));
+  document.addEventListener('mousedown', () => isMouseDown = true);
+  document.addEventListener('mouseup', () => isMouseDown = false);
   slider.addEventListener('input', () => {
-    gridContainer.innerHTML = '';
-    createGrid(slider.value, colourSelection)
-    sliderValue.textContent = slider.value
+    createGrid(slider.value);
+    sliderValue.textContent = slider.value;
   })
 
-  resetGridButton.addEventListener('click', () => {
-    gridContainer.innerHTML = '';
-    createGrid(slider.value, colourSelection)
-    sliderValue.textContent = slider.value
-  })
-
-  randomColour.addEventListener('click', () => createGridRandom(slider.value))
+  // On page load (default grid size is 16)
+  createGrid(slider.value);
